cbb1a1336fa3e8c68affd1b6a4e487726df86860
JagneetJSG <jagneetsinghgill2@gmail.com>
2025-06-10 23:10:17 +0530

    update the connection URI

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDb = async () => {
   await mongoose.connect(
-    process.env.MONGODB_CONNECTION_STRING
+    `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@namastedev.vqli0dy.mongodb.net/`
   );
 };
 
